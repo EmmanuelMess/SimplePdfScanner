cb3b42d8ffa8f064cfded2ef8e267ff356eac5d8
EmmanuelMess <emmanuelbendavid@gmail.com>
2020-10-07 16:43:06 -0300

    Detect edges

--- simple_pdf_scanner/android/app/src/main/java/com/emmanuelmess/simple_pdf_scanner/processing/MainProcessor.kt
+++ simple_pdf_scanner/android/app/src/main/java/com/emmanuelmess/simple_pdf_scanner/processing/MainProcessor.kt
@@ -2,10 +2,14 @@ package com.emmanuelmess.simple_pdf_scanner.processing
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import org.opencv.android.OpenCVLoader
 import org.opencv.android.Utils
 import org.opencv.core.Mat
+import org.opencv.core.MatOfPoint
+import org.opencv.core.Size
 import org.opencv.imgproc.Imgproc
+import org.opencv.imgproc.Imgproc.*
 import java.io.ByteArrayOutputStream
 
 
@@ -20,18 +24,36 @@ object MainProcessor {
         val img = Mat()
         Utils.bitmapToMat(bitmap, img)
         
-        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA)
-
-        val imgResult: Mat = img.clone()
-        Imgproc.Canny(img, imgResult, 80.0, 90.0)
+        val imgResult = work(img)
         
         val imgBitmap = Bitmap.createBitmap(imgResult.cols(), imgResult.rows(), Bitmap.Config.ARGB_8888)
         Utils.matToBitmap(imgResult, imgBitmap)
 
+        val rotated = Bitmap.createBitmap(
+                imgBitmap, 0, 0, imgBitmap.width, imgBitmap.height,
+                Matrix().apply { postRotate(90f) }, true)
+
         val byteArrayOutputStream = ByteArrayOutputStream()
-        imgBitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+        rotated.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
         val byteArray = byteArrayOutputStream.toByteArray()
 
         return byteArray
+    }
+
+    private fun work(input: Mat): Mat {
+        val resizedInput = Mat()
+
+        val r = 500.0 / input.height()
+        val width = (input.width() * r).toInt()
+        resize(input, resizedInput, Size(width.toDouble(), 500.0))
+
+        val grey = Mat()
+        cvtColor(resizedInput, grey, COLOR_RGB2GRAY)
+        GaussianBlur(grey, grey, Size(5.0, 5.0), 0.0)
+        
+        val edged = Mat()
+        Canny(grey, edged, 75.0, 200.0)
+
+        return edged
     }
 }
